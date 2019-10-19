b3007260b84937719a1b03fc15cd7fea5a7e1b71
arpitkogta <54179887+arpitkogta@users.noreply.github.com>
2019-10-20 01:05:29 +0530

    Update primefactorization.py
    bug in line 7 for i=0  
    if i is initialised to 1

--- Prime Factors/primefactorization.py
+++ Prime Factors/primefactorization.py
@@ -1,10 +1,10 @@
 n=int(input("Enter an integer:"))
 print("Factors are:")
-i=0
-for(i=0;i<n;i++)
+i=1 #i initialised to 1 instead of 0
+#for(i=0;i<n;i++) this line of code is not required as the while loop is already there for iteration of the code
 while(i<=n):
     k=0
-    if(n%i==0):
+    if(n%i==0): #bug in this line of code for i=0
         j=1
         while(j<=i):
             if(i%j==1):
